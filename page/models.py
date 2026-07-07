de61719ab04bbc3ce81cd2170a2e287c4f1a47bd
Prakash Chandra <prakashworks98@gmail.com>
2026-07-07 12:09:01 +0545

    added the code for the database installed the pillow to upload the professionals image and configured the setting to store the images

--- page/models.py
+++ page/models.py
@@ -1,3 +1,34 @@
 from django.db import models
 
+
 # Create your models here.
+# create the model for the post
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    body = models.TextField()
+    author = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.title
+
+
+# create the model for the contact details
+class Contact(models.Model):
+    phone = models.CharField(max_length=15)
+    location = models.CharField(max_length=100)
+    email = models.EmailField()
+
+    def __str__(self):
+        """returns the location as the string so that i can be seen on the admin panel"""
+        return self.location
+
+
+# create the model to store the professionals details
+class Professionals(models.Model):
+    name = models.CharField(max_length=100)
+    position = models.CharField(max_length=100)
+    image = models.ImageField(upload_to="professionals/")
+
+    def __str__(self):
+        """returns the name of the employee so that it can be seen of the website"""
+        return self.name
