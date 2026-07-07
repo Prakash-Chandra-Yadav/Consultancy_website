fb9e229666fdc5ca111ec5a21388de44ceb355cc
Prakash Chandra <prakashworks98@gmail.com>
2026-07-07 12:33:45 +0545

    fix:fixed the code of the view

--- page/views.py
+++ page/views.py
@@ -5,5 +5,5 @@ from .models import Contact, Professionals, Post
 # Create your views here.
 # create the view for teh contact page first
 def contactview(request):
-    posts = Post.objects.all()
-    return render(request, "contact.html", {"posts": posts})
+    contacts = Contact.objects.all()
+    return render(request, "contact.html", {"contacts": contacts})
