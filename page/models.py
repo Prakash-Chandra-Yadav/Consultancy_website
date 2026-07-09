fba8e7f46fd814c0d457fe60752b7adf120b3923
Prakash Chandra <prakashworks98@gmail.com>
2026-07-09 14:06:28 +0545

    feat:added the absolute url method in the models

--- page/models.py
+++ page/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.utils import timezone
+from django.urls import reverse
 import datetime
 
 
@@ -19,6 +20,9 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
+    def get_absolute_url(self):
+        return reverse("project_details", kwargs={"pk": self.pk})
 
 
 # create the model for the contact details
